21cfa6c85c368abf176d913acbd507cdc16ca144
realjessechou <jessechou123@163.com>
2024-06-01 17:06:25 +0800

    新增营业额数据统计

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -9,6 +9,7 @@ import org.apache.ibatis.annotations.SelectKey;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface OrderMapper {
@@ -63,4 +64,10 @@ public interface OrderMapper {
     @Select("select * from orders where status = #{status} and order_time < #{orderTime}")
     List<Orders> getByStatusAndOrderTimeLT(Integer status, LocalDateTime orderTime);
 
+    /**
+     * 统计营业额
+     * @param map
+     * @return
+     */
+    Double sumByMap(Map map);
 }
